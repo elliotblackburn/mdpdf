b11ce04f3f53dd01886dc73e7a12cd31061c97f8
Ryo Ota <nwtgck@gmail.com>
2018-01-23 00:54:37 +0900

    Use singlequotes instead of douglequotes

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function getAllStyles(options) {
 
 	// CSS in .config/mdpdf/style.css
 	if (typeof process.env.HOME !== 'undefined') {
-		const dotConfigCssPath = path.join(process.env.HOME, ".config", "mdpdf", "style.css");
+		const dotConfigCssPath = path.join(process.env.HOME, '.config', 'mdpdf', 'style.css');
 		if (fs.existsSync(dotConfigCssPath)) {
 			cssStyleSheets.push(dotConfigCssPath);
 		}
