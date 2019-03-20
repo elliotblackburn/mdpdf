6df9f565ba64337bb41c075dde6ff39eb5eaf7ca
Elliot Blackburn <elliot.blackburn@just-eat.com>
2019-03-20 15:46:19 +0000

    allow margins to be unspecified via JS api

--- src/puppeteer-helper.js
+++ src/puppeteer-helper.js
@@ -4,17 +4,20 @@ function getOptions(options) {
     displayHeaderFooter = true;
   }
 
+  let margin = {};
+  if (options.pdf.border) {
+    margin.top = options.pdf.border.top || undefined;
+    margin.right = options.pdf.border.right || undefined;
+    margin.bottom = options.pdf.border.bottom || undefined;
+    margin.left = options.pdf.border.left || undefined;
+  }
+
   return {
     path: options.destination,
     displayHeaderFooter: false,
     printBackground: true,
     format: options.pdf.format,
-    margin: {
-      top: options.pdf.border.top,
-      right: options.pdf.border.right,
-      bottom: options.pdf.border.bottom,
-      left: options.pdf.border.left,
-    },
+    margin,
     displayHeaderFooter,
     headerTemplate: options.header || '',
     footerTemplate: options.footer || '',
